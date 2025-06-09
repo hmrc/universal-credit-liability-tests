d47033996b5228cab31fefb76d97249982d1cba1
dananjaiahnemmadi <202163272+dananjaiahnemmadi@users.noreply.github.com>
2025-06-09 17:41:03 +0100

    Created different scenarios for 204, 400, 403 requests

---
+++ src/test/scala/uk/gov/hmrc/dwp/api/specs/PositiveScenarios.scala
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.dwp.api.specs
+
+import org.scalatest.matchers.must.Matchers.mustBe
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.http.Status
+import uk.gov.hmrc.dwp.api.service.ApiService
+import uk.gov.hmrc.dwp.api.testData.*
+
+class PositiveScenarios extends BaseSpec with GuiceOneServerPerSuite with TestDataFile {
+  Feature("Positive Scenario") {
+
+    // 204 Request
+    Scenario("UCL_TC_001_0.1: Insert - LCW/LCWRA") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response = ApiService().postNotification(insertURL, validHeaders, validInsertLCWLCWRALiabilityRequest)
+      println(response.body)
+
+      Then("204 No content should display")
+      response.status mustBe Status.NO_CONTENT
+    }
+
+    Scenario("UCL_TC_001_0.0.1: Insert - LCW/LCWRA with No End Date") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response =
+        ApiService().postNotification(insertURL, validHeaders, validInsertLCWLCWRALiabilityRequestNOEndDate)
+
+      Then("204 No content should display")
+      response.status mustBe Status.NO_CONTENT
+    }
+
+    Scenario("UCL_TC_001_0.2: Insert - UC") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response = ApiService().postNotification(insertURL, validHeaders, validInsertUCLiabilityRequest)
+
+      Then("204 No content should display")
+      response.status mustBe Status.NO_CONTENT
+    }
+
+    Scenario("UCL_TC_001_0.3: Terminate - LCW/LCWRA") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response =
+        ApiService().postNotification(terminationURL, validHeaders, validTerminationLCWLCWRALiabilityRequest)
+
+      Then("204 No content should display")
+      response.status mustBe Status.NO_CONTENT
+    }
+
+    Scenario("UCL_TC_001_0.4: Terminate - UC") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response = ApiService().postNotification(terminationURL, validHeaders, validTerminationUCLiabilityRequest)
+
+      Then("204 No content should display")
+      response.status mustBe Status.NO_CONTENT
+    }
+
+    // 400 Request
+    Scenario("UCL_TC_001_0.5: Insert Invalid details - LCW/LCWRA") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response = ApiService().postNotification(insertURL, validHeaders, invalidInsertLCWLCWRALiabilityRequest)
+
+      Then("400 No content should display")
+      response.status mustBe Status.BAD_REQUEST
+    }
+
+    Scenario("UCL_TC_001_0.6: Insert Invalid details - UC") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response = ApiService().postNotification(insertURL, validHeaders, invalidInsertUCLiabilityRequest)
+
+      Then("400 No content should display")
+      response.status mustBe Status.BAD_REQUEST
+    }
+
+    Scenario("UCL_TC_001_0.7: Termination Invalid details - LCW/LCWRA") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response =
+        ApiService().postNotification(terminationURL, validHeaders, inValidTerminationLCWLCWRALiabilityRequest)
+
+      Then("400 No content should display")
+      response.status mustBe Status.BAD_REQUEST
+    }
+
+    Scenario("UCL_TC_001_0.8: Termination Invalid details - UC") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response = ApiService().postNotification(terminationURL, validHeaders, inValidTerminationUCLiabilityRequest)
+
+      Then("400 No content should display")
+      response.status mustBe Status.BAD_REQUEST
+    }
+
+    // 403 Request
+    Scenario("UCL_TC_001_0.9: Insert Invalid Headers details - UC") {
+      Given("The Universal Credit API is up and running")
+      When("A request is sent")
+
+      val response = ApiService().postNotification(insertURL, invalidHeaders, invalidInsertUCLiabilityRequest)
+
+      Then("403 No content should display")
+      response.status mustBe Status.FORBIDDEN
+    }
+  }
+}
---
+++ src/test/scala/uk/gov/hmrc/dwp/api/testData/TestDataFile.scala
@@ -0,0 +1,129 @@
+package uk.gov.hmrc.dwp.api.testData
+
+import play.api.libs.json.{JsValue, Json}
+
+import scala.util.Random
+
+trait TestDataFile {
+  val baseUrl = s"http://localhost:16108"
+
+  val randomNino: String = "AB%06d".format(Random.nextInt(999999))
+
+  val insertURL: String =
+    s"$baseUrl/universal-credit-liability-stubs/person/$randomNino/liability/universal-credit"
+
+  val terminationURL: String =
+    s"$baseUrl/universal-credit-liability-stubs/person/$randomNino/liability/universal-credit/termination"
+
+  val validHeaders: Seq[(String, String)] =
+    Seq(
+      "Content-Type"         -> "application/json",
+      "correlationId"        -> "3e8dae97-b586-4cef-8511-68ac12da9028",
+      "gov-uk-originator-id" -> "gov-uk-originator-id"
+    )
+
+  val invalidHeaders: Seq[(String, String)] =
+    Seq(
+      "Content-Type"  -> "application/json",
+      "correlationId" -> "3e8dae97-b586-4cef-8511-68ac12da9028"
+    )
+
+  val validInsertLCWLCWRALiabilityRequestNOEndDate: JsValue =
+    Json.parse("""
+        |{
+        |  "universalCreditLiabilityDetails": {
+        |    "universalCreditRecordType": "LCW/LCWRA",
+        |    "dateOfBirth": "2002-10-10",
+        |    "liabilityStartDate": "2015-08-19"
+        |  }
+        |}
+        |""".stripMargin)
+
+  val validInsertLCWLCWRALiabilityRequest: JsValue =
+    Json.parse("""
+        |{
+        |  "universalCreditLiabilityDetails": {
+        |    "universalCreditRecordType": "LCW/LCWRA",
+        |    "dateOfBirth": "2002-10-10",
+        |    "liabilityStartDate": "2015-08-19",
+        |    "liabilityEndDate": "2025-01-04"
+        |  }
+        |}
+        |""".stripMargin)
+
+  val validInsertUCLiabilityRequest: JsValue =
+    Json.parse("""
+        |{
+        |  "universalCreditLiabilityDetails": {
+        |    "universalCreditRecordType": "UC",
+        |    "dateOfBirth": "2002-10-10",
+        |    "liabilityStartDate": "2015-08-19",
+        |    "liabilityEndDate": "2025-01-04"
+        |  }
+        |}
+        |""".stripMargin)
+
+  val validTerminationLCWLCWRALiabilityRequest: JsValue =
+    Json.parse("""
+        |{
+        |  "ucLiabilityTerminationDetails": {
+        |    "universalCreditRecordType": "LCW/LCWRA",
+        |    "liabilityStartDate": "2015-08-19",
+        |    "liabilityEndDate": "2025-01-04"
+        |  }
+        |}
+        |""".stripMargin)
+
+  val validTerminationUCLiabilityRequest: JsValue =
+    Json.parse("""
+        |{
+        |  "ucLiabilityTerminationDetails": {
+        |    "universalCreditRecordType": "UC",
+        |    "liabilityStartDate": "2015-08-19",
+        |    "liabilityEndDate": "2025-01-04"
+        |  }
+        |}
+        |""".stripMargin)
+
+  val invalidInsertLCWLCWRALiabilityRequest: JsValue =
+    Json.parse("""
+        |{
+        |  "universalCreditLiabilityDetails": {
+        |    "universalCreditRecordType": "LCW/LCWRA",
+        |    "dateOfBirth": "2002-10-10",
+        |    "liabilityEndDate": "2025-01-04"
+        |  }
+        |}
+        |""".stripMargin)
+
+  val invalidInsertUCLiabilityRequest: JsValue =
+    Json.parse("""
+        |{
+        |  "universalCreditLiabilityDetails": {
+        |    "universalCreditRecordType": "UC",
+        |    "dateOfBirth": "2002-10-10",
+        |    "liabilityEndDate": "2025-01-04"
+        |  }
+        |}
+        |""".stripMargin)
+
+  val inValidTerminationLCWLCWRALiabilityRequest: JsValue =
+    Json.parse("""
+        |{
+        |  "ucLiabilityTerminationDetails": {
+        |    "universalCreditRecordType": "LCW/LCWRA",
+        |    "liabilityStartDate": "2015-08-19"
+        |  }
+        |}
+        |""".stripMargin)
+
+  val inValidTerminationUCLiabilityRequest: JsValue =
+    Json.parse("""
+        |{
+        |  "ucLiabilityTerminationDetails": {
+        |    "universalCreditRecordType": "UC",
+        |    "liabilityStartDate": "2015-08-19"
+        |  }
+        |}
+        |""".stripMargin)
+}
