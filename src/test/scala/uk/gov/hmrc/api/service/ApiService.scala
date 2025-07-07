c80f1c6ebcfde5a26e3d202f9e7bf00ecf261eec
Paul Olabisi <18728756+paulonevrything@users.noreply.github.com>
2025-07-07 17:24:58 +0100

    Refactored code for modularity

--- src/test/scala/uk/gov/hmrc/api/service/ApiService.scala
+++ src/test/scala/uk/gov/hmrc/api/service/ApiService.scala
@@ -16,80 +16,39 @@
 
 package uk.gov.hmrc.api.service
 
-import org.apache.pekko.actor.ActorSystem
 import play.api.libs.json.JsValue
-import play.api.libs.ws.ahc.StandaloneAhcWSClient
-import play.api.libs.ws.{StandaloneWSResponse, writeableOf_JsValue}
+import play.api.libs.ws.StandaloneWSResponse
+import uk.gov.hmrc.api.client.HttpClient
+import uk.gov.hmrc.api.conf.TestEnvironment
+import uk.gov.hmrc.api.helpers.AuthHelper
 
-import java.net.URI
-import java.net.http.HttpRequest.BodyPublishers
-import java.net.http.HttpResponse.BodyHandlers
-import java.net.http.{HttpClient, HttpRequest}
-import java.time.Duration.ofSeconds
-import java.util.UUID
 import scala.concurrent.Await
 import scala.concurrent.duration.*
-import scala.jdk.OptionConverters.RichOptional
 
-class ApiService {
-  implicit val actorSystem: ActorSystem = ActorSystem()
-  val wsClient: StandaloneAhcWSClient   = StandaloneAhcWSClient()
-  val http: HttpClient                  = HttpClient.newHttpClient()
+class ApiService extends HttpClient {
 
-  val authApiUrl = s"http://localhost:8585/application/session/login"
-
-  def postNotification(
-    url: String,
-    headers: Seq[(String, String)],
-    requestBody: JsValue,
-    authToken: String
-  ): StandaloneWSResponse = {
-
-    val updatedHeaders = headers :+ ("authorization" -> s"$authToken")
-    val response       = wsClient
-      .url(url)
-      .withHttpHeaders(updatedHeaders: _*)
-      .withBody(requestBody)
-      .execute("POST")
-
-    Await.result(response, 10.seconds)
-  }
+  val host: String = TestEnvironment.url("ucl")
+  val authHelper   = new AuthHelper
 
   def postNotificationWithValidToken(
     url: String,
     headers: Seq[(String, String)],
     requestBody: JsValue
   ): StandaloneWSResponse = {
-    val authToken = getAuthToken
-    postNotification(url, headers, requestBody, authToken)
+
+    val token = authHelper.retrieveAuthBearerToken()
+    makeRequest(headers, requestBody, token)
   }
 
-  def getAuthToken: String = {
-    val request = HttpRequest
-      .newBuilder()
-      .uri(new URI(authApiUrl))
-      .header("content-type", "application/json")
-      .POST(
-        BodyPublishers.ofString(
-          s"""
-             |{
-             | "clientId": "${UUID.randomUUID().toString}",
-             | "authProvider": "PrivilegedApplication",
-             | "applicationId": "${UUID.randomUUID().toString}",
-             | "applicationName": "universal-credit-liability-api",
-             | "enrolments": [],
-             | "ttl": 60000
-             |}
-             |""".stripMargin
-        )
-      )
-      .timeout(ofSeconds(5, 0))
-      .build()
-
-    val response = http.send(request, BodyHandlers.ofString())
-
-    require(response.statusCode() == 201, "Unable to create auth token")
-
-    response.headers().firstValue("Authorization").toScala.get
+  def makeRequest(headers: Seq[(String, String)], requestBody: JsValue, token: String): StandaloneWSResponse = {
+    val url: String = s"$host/notification"
+    Await.result(
+      post(
+        url,
+        requestBody.toString,
+        headers :+ ("authorization" -> s"$token")
+      ),
+      10.seconds
+    )
   }
 }
--- src/test/scala/uk/gov/hmrc/api/specs/ErrorValidation_UnauthorizedToken.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/ErrorValidation_UnauthorizedToken.scala
@@ -3,7 +3,6 @@ package uk.gov.hmrc.api.specs
 import org.scalatest.matchers.must.Matchers.mustBe
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.http.Status
-import uk.gov.hmrc.api.service.ApiService
 import uk.gov.hmrc.api.testData.TestDataFile
 
 class ErrorValidation_UnauthorizedToken extends BaseSpec with GuiceOneServerPerSuite with TestDataFile {
@@ -13,8 +12,8 @@ class ErrorValidation_UnauthorizedToken extends BaseSpec with GuiceOneServerPerS
       Given("The Universal Credit API is up and running")
       When("A Invalid token is sent")
 
-      val response = ApiService()
-        .postNotification(endpointUrl, validHeaders, validInsertLCWLCWRALiabilityRequest, getInvalidAuthToken)
+      val response = apiService
+        .makeRequest(validHeaders, validInsertLCWLCWRALiabilityRequest, getInvalidAuthToken)
 
       Then("401 Unauthorized received")
       response.status mustBe Status.UNAUTHORIZED
@@ -24,8 +23,8 @@ class ErrorValidation_UnauthorizedToken extends BaseSpec with GuiceOneServerPerS
       Given("The Universal Credit API is up and running")
       When("A Empty token is sent")
 
-      val response = ApiService()
-        .postNotification(endpointUrl, validHeaders, validInsertLCWLCWRALiabilityRequest, getNoAuthToken)
+      val response = apiService
+        .makeRequest(validHeaders, validInsertLCWLCWRALiabilityRequest, getNoAuthToken)
 
       Then("401 Unauthorized received")
       response.status mustBe Status.UNAUTHORIZED
@@ -35,8 +34,8 @@ class ErrorValidation_UnauthorizedToken extends BaseSpec with GuiceOneServerPerS
       Given("The Universal Credit API is up and running")
       When("A Empty token is sent")
 
-      val response = ApiService()
-        .postNotification(endpointUrl, validHeaders, validInsertLCWLCWRALiabilityRequest, getExpiredAuthToken)
+      val response = apiService
+        .makeRequest(validHeaders, validInsertLCWLCWRALiabilityRequest, getExpiredAuthToken)
 
       Then("401 Unauthorized received")
       response.status mustBe Status.UNAUTHORIZED
