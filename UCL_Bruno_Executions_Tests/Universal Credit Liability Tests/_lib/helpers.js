0d0487e087fa9fbed9eb95db17db53b11f2e4744
Nikolas <gianick@users.noreply.github.com>
2026-01-27 21:45:52 +0000

    Restructured all requests

--- UCL_Bruno_Executions_Tests/Universal Credit Liability Tests/_lib/helpers.js
+++ UCL_Bruno_Executions_Tests/Universal Credit Liability Tests/_lib/helpers.js
@@ -1,11 +1,17 @@
 const randomNino = () => {
-    const number = `${Math.floor(Math.random() * 100000)}`.padStart(6, '0');
-    return `AA${number}`;
+  const number = `${Math.floor(Math.random() * 100000)}`.padStart(6, '0');
+  return `AA${number}`;
 }
+
+// TODO: get a random between UC and LCW/LCWRA
+const randomUniversalCreditRecordType = 'UC';
+
+// TODO: get a random between Insert and Terminate
+const randomUniversalCreditAction = 'Insert';
 
 const invalidNino =  'AB123456788';
 
 module.exports = {
-    randomNino,
-    invalidNino
+  randomNino,
+  invalidNino
 };
