95d250d32bfc759a0f2caf9700d3815d48cbe1fb
Nikolas <gianick@users.noreply.github.com>
2026-01-28 01:12:41 +0000

    Added helper methods - used them in 204

--- UCL_Bruno_Executions_Tests/Universal Credit Liability Tests/_lib/helpers.js
+++ UCL_Bruno_Executions_Tests/Universal Credit Liability Tests/_lib/helpers.js
@@ -1,17 +1,34 @@
+const getRandomElement = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 const randomNino = () => {
   const number = `${Math.floor(Math.random() * 100000)}`.padStart(6, '0');
   return `AA${number}`;
 }
 
-// TODO: get a random between UC and LCW/LCWRA
-const randomUniversalCreditRecordType = 'UC';
+const randomUniversalCreditRecordType = () => {
+  return getRandomElement(['UC', 'LCW/LCWRA']);
+}
 
-// TODO: get a random between Insert and Terminate
-const randomUniversalCreditAction = 'Insert';
+const randomUniversalCreditAction = () => {
+  return getRandomElement(['Insert', 'Terminate']);
+}
 
-const invalidNino =  'AB123456788';
+const randomGovUkOriginatorId = () => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const length = Math.floor(Math.random() * 38) + 3; // 3 to 40 characters long
+  return Array.from({ length })
+      .map(() => chars[Math.floor(Math.random() * chars.length)])
+      .join('');
+}
+
+const invalidNino = 'QQ1234567890';
 
 module.exports = {
   randomNino,
+  randomGovUkOriginatorId,
+  randomUniversalCreditRecordType,
+  randomUniversalCreditAction,
   invalidNino
 };
