08e4f5cb95cfa9f922c3a958a515d4b996fb494e
hitesh-digital <hitesh.sanghani@digital.hmrc.gov.uk>
2026-03-11 16:22:49 +0000

    'added acceptance test for error code 65538'

--- src/test/scala/uk/gov/hmrc/api/specs/B001InsertBusinessScenario.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/B001InsertBusinessScenario.scala
@@ -105,6 +105,12 @@ class B001InsertBusinessScenario
         "End date before start date"
       ),
       (
+        "Error : API return 422 and cascade business error 65537 from HIP to DWP",
+        "EA040",
+        "65538",
+        "End date missing but the input was a Termination"
+      ),
+      (
         "Error : API return 422 and cascade business error 65541 from HIP to DWP",
         "BX100",
         "65541",
