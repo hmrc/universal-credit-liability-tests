c0d0b20c77ab70f65bc71880c1d202aea5e70582
hitesh-digital <hitesh.sanghani@digital.hmrc.gov.uk>
2026-03-11 17:09:13 +0000

    'Minor update for insert error code 65538'

--- src/test/scala/uk/gov/hmrc/api/specs/B001InsertBusinessScenario.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/B001InsertBusinessScenario.scala
@@ -105,7 +105,7 @@ class B001InsertBusinessScenario
         "End date before start date"
       ),
       (
-        "Error : API return 422 and cascade business error 65537 from HIP to DWP",
+        "Error : API return 422 and cascade business error 65538 from HIP to DWP",
         "EA040",
         "65538",
         "End date missing but the input was a Termination"
