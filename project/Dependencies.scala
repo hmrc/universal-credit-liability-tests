f62350f6c5bb96b3f5910b8b4d03dff4fa32ab49
dmcglinchey <166512634+DMcGlinchey@users.noreply.github.com>
2026-03-18 17:00:19 +0000

    Further updates to the libraries

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "api-test-runner"             % "0.10.0" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"          % "7.0.2"  % Test,
-    "org.playframework"      %% "play-ws"                     % "3.0.9"  % Test,
+    "org.playframework"      %% "play-ws"                     % "3.0.10" % Test,
     "org.apache.pekko"       %% "pekko-actor"                 % pekkoVersion,
     "org.apache.pekko"       %% "pekko-stream"                % pekkoVersion,
     "org.apache.pekko"       %% "pekko-protobuf-v3"           % pekkoVersion,
